73daed17bf58012893f2932ec933b9ae6754f90f
Dariel <darielriverabetancourt02@gmail.com>
2025-11-27 22:22:09 -0500

    Crear la clase Member, declarar sus variables y crear el constructor

---
+++ src/models/member.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+class Member:
+    telegram_id: int
+    username: str
+    name: str
+    joined_at: datetime
+    
+    def __init__(self, telegram_id, username, name, joined_at=None):
+        self.telegram_id = telegram_id
+        self.username = username
+        self.name = name
+        self.joined_at = joined_at if joined_at else datetime.now()

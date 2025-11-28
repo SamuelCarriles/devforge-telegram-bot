a4ba95a16a89e7a2f363653f0abf0cecae7e0e57
Isaias Cruz <isaiascruzmaister5@gmail.com>
2025-11-27 22:24:47 -0500

    Crear  constructor

--- src/models/task.py
+++ src/models/task.py
@@ -3,9 +3,19 @@ from datetime import datetime
 class Task:
  
  id: int
- 
  title: str
-
  description: str
-
  due_date: datetime
+ completed: bool
+ create_at: datetime
+ completed_at: datetime
+
+ def __init__(self, id, title, description=None, due_date=None, completed=False, created_at=None, completed_at=None):
+   self.id=id
+   self.title=title
+   self.description=description
+   self.due_date=due_date
+   self.completed=completed
+   self.create_at=completed_at
+   self.completed_at=completed_at
+  

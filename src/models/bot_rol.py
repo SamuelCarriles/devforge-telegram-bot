1b063c0109cf420789793bb735cbcf9f177c78ac
YarielZC <yarielzamoracueto@gmail.com>
2025-11-30 01:20:35 -0500

    Formatear el código en los archivos commands.py y bot_rol.py para mejorar la legibilidad y la consistencia en el estilo.

--- src/models/bot_rol.py
+++ src/models/bot_rol.py
@@ -1,5 +1,6 @@
 from src.logic.commands import clean_command
 from src.utils.command_permissions import COMMAND_ALLOWLIST
+
 class BotRol:
     key: str
     name: str
@@ -23,40 +24,40 @@ class BotRol:
 
     def to_dict(self) -> dict:
         return {
-            'key': self.key,
-            'name': self.name,
-            'hierarchy': self.hierarchy,
+            "key": self.key,
+            "name": self.name,
+            "hierarchy": self.hierarchy,
         }
     
     @classmethod
-    def from_dict(cls, data: dict) -> 'BotRol':
+    def from_dict(cls, data: dict) -> "BotRol":
         
         return cls(
-            key=data['key'],
-            name=data['name'],
-            hierarchy=data['hierarchy'],
+            key=data["key"],
+            name=data["name"],
+            hierarchy=data["hierarchy"],
         )
 
 class Admin(BotRol):
 
     def __init__(self, hierarchy: int = 100) -> None:
         super().__init__(
-            key='admin', 
-            name='Administrador', 
+            key="admin", 
+            name="Administrador", 
             hierarchy=hierarchy)
         
 class Member(BotRol):
 
     def __init__(self, hierarchy: int = 25) -> None:
         super().__init__(
-            key='member', 
-            name='Miembro', 
+            key="member", 
+            name="Miembro", 
             hierarchy=hierarchy)
 
 class Guest(BotRol):
 
     def __init__(self) -> None:
         super().__init__(
-            key='guest', 
-            name='Invitado', 
+            key="guest", 
+            name="Invitado", 
             hierarchy=1)
